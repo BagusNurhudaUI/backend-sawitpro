6afa62d7a4aa9ecc7a36ddec1c97174d823727b3
Bagus <bagus.nurhuda@ui.ac.id>
2023-03-30 15:45:07 +0700

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,98 @@
+import org.json.JSONObject;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+public class App 
+{
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String urlImage1 = "https://lh3.googleusercontent.com/MLze3lbJGhRvW7_uweMbSeu7dGlbNyJxrGHjZQS_qHZ4vD2lYdA9SStZGdB98gY2RCs=w2400";
+        String urlImage2 = "https://lh5.googleusercontent.com/W9jmYbsTl1Ug2Xi7bKpSqACD4c1-Ku4dmGbebKvpCoDYIomCBv0kXNBckAmxZV_xrug=w2400";
+        String urlImage3 = "https://lh4.googleusercontent.com/rrJ9_c_4jcUu6zRGEwN6qX_Zq-0E8fpRbHNVHsLIWLwu-db4c8lpBVMEmp9elUs4jBw=w2400";
+        String urlImage4 = "https://lh6.googleusercontent.com/eVnDc43gE2ni3hmm4lbrmEiuMqPTmwMBXqzxCHp-NdfztvTJIS9Ij9Zcpl8iN_c3I0I=w2400";
+        ocrApi(urlImage3);
+    }
+
+    public static void ocrApi(String url) throws IOException, InterruptedException {
+        try {
+            var httpClient = HttpClient.newBuilder().build();
+            JSONObject payload = new JSONObject();
+            payload.put("providers", "google");
+            payload.put("language", "en");
+            payload.put("file_url", url);
+            var host = "https://api.edenai.run";
+            var pathname = "/v2/ocr/ocr";
+            var request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(String.valueOf(payload)))
+                    .uri(URI.create(host + pathname ))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMmVkYzhhNjctYjUzMi00M2UwLWIzMDAtZGFmNWQyMGFhNDBhIiwidHlwZSI6ImFwaV90b2tlbiJ9.dC00j9FYjflPiTRfOdVnaywi-AAqqjJvEOGC_YqlUOs")
+                    .build();
+
+            var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200){
+                JSONObject res = new JSONObject(response.body());
+                res = (JSONObject) res.getJSONObject("google");
+                String result = res.getString("text");
+                System.out.println(result);
+
+                if(result.matches("(?s)([\\P{IsHan}]*[\\p{IsHan}]){8}.*")) {
+                    // Accept name
+                    System.out.println("china");
+                    PrintWriter out = new PrintWriter("china.txt");
+                    out.println(result);
+                    out.close();
+                }
+                else {
+                    // Ask to enter again
+                    System.out.println("english");
+                    PrintWriter out = new PrintWriter("english.txt");
+                    out.println(result);
+                    out.close();
+                    toImage(result);
+                }
+            } else {
+                System.out.println(response.body());
+                throw new IOException("Error in response API");
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void toImage(String result) throws  IOException {
+        String str = result;
+        BufferedImage bufferedImage = new BufferedImage(1200, 700, BufferedImage.TYPE_INT_RGB);
+        Graphics g = bufferedImage.createGraphics();
+        g.setFont(new Font("TimesRoman", Font.BOLD, 24));
+
+        java.util.List<String> myList = List.of(str.split(" "));
+        int i = 10;
+        int y = 20;
+        for (String s: myList) {
+            ((Graphics2D) g).setPaint(Color.WHITE);
+            if(s.matches("(\\w+(o|O)\\w+)|(\\w+(o|O))|((o|O)\\w+)")){
+                ((Graphics2D) g).setPaint(Color.BLUE);
+            }
+            g.drawString(s, i, y);
+            i=i+(s.length()*17 + 5);
+            if(i>=1000) {
+                i = 10;
+                y = y + 30;
+            }
+        }
+        ImageIO.write(bufferedImage, "png", new File("result.png"));
+    }
+}
---
+++ src/main/java/Upload.java
@@ -0,0 +1,115 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.*;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/* class to demonstarte use of Drive files list API */
+public class Upload {
+    private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES =
+            Collections.singletonList(DriveScopes.DRIVE_METADATA);
+    private static final String CREDENTIALS_FILE_PATH = "src\\main\\resources\\credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT)
+            throws IOException {
+        InputStream in = Upload.class.getClass().getResourceAsStream(CREDENTIALS_FILE_PATH);
+//        InputStream in = new FileInputStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        System.out.println(credential);
+        return credential;
+    }
+
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+
+        // Print the names and IDs for up to 10 files.
+//        FileList result = service.files().list()
+//                .setPageSize(10)
+//                .setFields("nextPageToken, files(id, name)")
+//                .execute();
+//        List<File> files = result.getFiles();
+//        if (files == null || files.isEmpty()) {
+//            System.out.println("No files found.");
+//        } else {
+//            System.out.println("Files:");
+//            for (File file : files) {
+//                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+//            }
+//        }
+        System.out.println(uploadBasic(service));
+    }
+        public static String uploadBasic(Drive service) throws IOException {
+            // Load pre-authorized user credentials from the environment.
+            // TODO(developer) - See https://developers.google.com/identity for
+            // guides on implementing OAuth2 for your application.
+            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                    .createScoped(Arrays.asList(DriveScopes.DRIVE_FILE));
+            HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(
+                    credentials);
+
+            // Build a new authorized API client service.
+            Drive service1 = service;
+            // Upload file photo.jpg on drive.
+            com.google.api.services.drive.model.File fileMetadata = new com.google.api.services.drive.model.File();
+            fileMetadata.setName("photo.jpg");
+            // File's content.
+            java.io.File filePath = new java.io.File("image/ImageWithWords1.jpg");
+            // Specify media type and file-path for file.
+            FileContent mediaContent = new FileContent("image/jpeg", filePath);
+            try {
+                com.google.api.services.drive.model.File file = service1.files().create(fileMetadata, mediaContent)
+                        .setFields("id")
+                        .execute();
+                System.out.println("File ID: " + file.getId());
+                return file.getId();
+            } catch (GoogleJsonResponseException e) {
+                // TODO(developer) - handle error appropriately
+                System.err.println("Unable to upload file: " + e.getDetails());
+                throw e;
+            }
+        }
+    }
+
